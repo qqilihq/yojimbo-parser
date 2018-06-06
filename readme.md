5d2e102f4945a737e7341bf32bb35d02901c2183
Philipp Katz <philipp@philippkatz.de>
2018-06-06 21:16:09 +0200

    Map CSV to 1Password's expected structure
    https://support.1password.com/create-csv-files/

--- readme.md
+++ readme.md
@@ -3,14 +3,16 @@ Yojimbo Parser
 
 Reads exported [Yojimbo](http://www.barebones.com/products/yojimbo/) passwords and serial numbers and converts them into CSV files. This way they can be easily imported to other software, such as [1Password](https://1password.com).
 
+The specification of the CSV file expected by 1Password can be found [here](https://support.1password.com/create-csv-files/).
+
 Usage
 -----
 
 1. Install dependencies using `yarn`
 2. Build the script using `yarn build`
 3. Use drag and drop to export the Yojimbo data to a folder in the Finder
-4. Run the script with `node built/parser path/to/folder path/to/file.csv`
-5. Do whatever you wish with the generated CSV file
+4. Run the script with `node built/parser path/to/folder path/to/file.csv entityType` (where `entityType` is either `password` or `serial`)
+5. Do whatever you wish with the generated CSV file (probably go to **1Password → Import → CSV**)
 
 - - -
 
--- src/parser.ts
+++ src/parser.ts
@@ -7,6 +7,42 @@ export interface ParseResult {
   [key: string]: string;
 }
 
+interface MapFunction {
+  (entry: ParseResult): ParseResult;
+}
+
+const mappings: { [entityType: string]: MapFunction } = {
+  password: entry => {
+    // title,website,username,password,notes,custom field 1,custom field 2
+    return {
+      title: entry['name'],
+      website: entry['location'],
+      username: entry['account'],
+      password: entry['password'],
+      notes: entry['comments']
+    };
+  },
+  serial: entry => {
+    // title,version,license key,your name,your email,company,download link,software publisher,publisher's website,retail price,support email,purchase date,order number,notes
+    return {
+      title: entry['productName'],
+      version: '',
+      license_key: entry['serialNumber'],
+      your_name: entry['ownerName'],
+      your_email: entry['emailAddress'],
+      company: entry['organization'],
+      download_link: '',
+      software_publisher: '',
+      publishers_website: '',
+      retail_price: '',
+      support_email: '',
+      purchase_date: '',
+      order_number: '',
+      notes: entry['comments']
+    };
+  }
+};
+
 // CLI only when module is not require'd
 if (require.main === module) {
 
@@ -14,9 +50,17 @@ if (require.main === module) {
   const args = process.argv.slice(2);
   const inputPath = args[0];
   const outputPath = args[1];
-  if (!inputPath || !outputPath) {
+  const entityType = args[2];
+  if (!inputPath || !outputPath || !entityType) {
     console.error('Expected input path as first paramter');
     console.error('Expected CSV output path as second parameter');
+    console.error('Expected entity type `password` or `serial` as third parameter');
+    process.exit(1);
+  }
+
+  const mapping = mappings[entityType];
+  if (!mapping) {
+    console.error(`No mapping for type '${entityType}'`);
     process.exit(1);
   }
 
@@ -30,10 +74,14 @@ if (require.main === module) {
     const fileContent = fs.readFileSync(filePath, { encoding: 'utf-8' });
     const parseResult = parse(fileContent);
     return parseResult;
-  }).filter(v => !!v);
+  }).filter(v => !!v) as ParseResult[];
 
-  // (2) write out the CSV file
-  const csv = json2csv.parse(entries);
+  // (2) map to the expected CSV structure
+  // https://support.1password.com/create-csv-files/
+  const mappedEntries = entries.map(mapping);
+
+  // (3) write out the CSV file
+  const csv = json2csv.parse(mappedEntries, { header: false });
   fs.writeFileSync(outputPath, csv);
 
 }
